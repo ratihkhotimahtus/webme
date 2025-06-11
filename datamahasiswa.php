42d1a911bb239e43ecb0377c44275a72e1847b6b
ratihkhotimahtus <ratihkhotimahtus@gmail.com>
2025-06-11 12:03:58 +0700

    nambahin database

--- datamahasiswa.php
+++ datamahasiswa.php
@@ -2,32 +2,23 @@
    // $nama = "Ratih Khotimahtus S";
    // echo "Hello World" . $nama;
 
-   $koneksi = mysqli_connect("localhost:3307","root", "", "webif");
-
-   if(!$koneksi)
-   {
-    die("Koneksi Gagal!".mysqli_connect_error());
-   }
-   else
-   {
-    echo "Koneksi Berhasil";
-   }
-
+   require 'function.php';
    $query = "SELECT * FROM mahasiswa";
-
-   $result = mysqli_query($koneksi, $query); //// object
+   $rows = query($query); /// hasilnya wadah dengan isinya
 
    /// ambil data (fetch) dari lemari result
+    /// while ($mhs = mysqli_fetch_assoc($result))
 
-    $mhs = mysqli_fetch_array($result);
-   ///mysqli_fetch_row() untuk array tipe numerik
+   /// mysqli_fetch_row() untuk array tipe numerik
    /// mysqli_fetch_assoc() untuk assosiatif
    /// mysqli_fetch_array()
    /// mysqli_fetch_object() pakai var_dump($mhs->nama);
-
-   var_dump($mhs["nama"]);
+    ///{
+      ///  var_dump($mhs);
+        /// kalo mau ditampilkan isi table tertentu var_dump($mhs["nim"]); 
+    ///}
+   
 ?>
-
 
 
 <!DOCTYPE html>
@@ -44,13 +35,25 @@
     <table border="1" cellspacing="0" cellpadding="10">
         <tr>
             <th>No</th>
+            <th>Foto</th>
             <th>Nama</th>
             <th>NIM</th>
             <th>Jurusan</th>
             <th>No Hp</th>
         </tr>
-
-
+        <?php
+        $i = 1;
+        foreach ($rows as $mhs) { ?>
+        <tr>
+            <td><?= $i ?></td>
+             <td><img src="images/<?=$mhs['foto'];?> "alt="<?=$mhs['nama'];?>"width="100"></td>
+            <td><?= $mhs["nama"]?></td>
+            <td><?= $mhs["nim"]?></td>
+            <td><?= $mhs["jurusan"]?></td>
+            <td><?= $mhs["nohp"]?></td>
+        </tr>
+        <?php $i++; } ?>
+       
     </table>
 </body>
 </html>
